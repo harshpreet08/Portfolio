48d9f8ede4fff4925c018aa2c8c9297f8a361e8b
Harshpreet Singh <96812198+harshpreet08@users.noreply.github.com>
2025-07-07 22:13:16 -0400

    Updated image and made small changes

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -6,13 +6,13 @@ import Button from "../Button";
 const Footer = ({}) => {
   return (
     <>
-      <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
+      <div className="mt-5 laptop:mt-20 p-2 laptop:p-0">
         <div>
           <h1 className="text-2xl text-bold pixelify-sans">Contact.</h1>
-          <div className="mt-10">
+          <div className="mt-3">
             <h1 className="mt-2 text-l laptop:text-xl w-full laptop:w-3/5 text-gray-500">
-              I&apos;m always happy to chat about robotics, technology, or
-              anything else on your mind—let&apos;s connect! 🚀
+              Whether you want to chat about backend systems, cloud tech, or the
+              latest in Formula 1, I&apos;m always open to a good conversation.
             </h1>
             {/* <div className="mt-5">
               <Button
